f425bdb318e88683b8ec0748cf96265456e94221
Михаил <91387796+StMihaile@users.noreply.github.com>
2022-12-25 13:47:22 +0400

    ver 1.0

---
+++ script/api.js
@@ -0,0 +1,63 @@
+  const CONFIG_API = {
+    url: 'https://cats.petiteweb.dev/api/single/catsstm',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
+};
+
+class Api {
+    constructor(config) {
+        this._url = config.url;
+        this._headers = config.headers;
+    }
+
+    _onResponce(res) {
+        return res.ok 
+        ? res.json() 
+        : Promise.reject({ ...res, message: "Ошибка на стороне сервера" })
+    }
+
+    getAllCats() {
+        return fetch(`${this._url}/show`, {
+            method: 'GET',
+        }).then(this._onResponce);
+    }
+
+    getAllIds() {
+        fetch(`${this._url}/ids`, {
+            method: "GET"
+        }).then(this._onResponce);
+    }
+
+    addNewCat(body) {
+        return fetch(`${this._url}/add`, {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify(body),
+        }).then(this._onResponce);
+    }
+
+    updateCatById(data, idCat) {
+        fetch(`${this._url}/update/${idCat}`, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: this._headers,
+        }).then(this._onResponce);
+    }
+
+    deleteCatById(idCat) {
+        fetch(`${this._url}/delete/${idCat}`, {
+            method: 'DELETE',
+        }).then(this._onResponce);
+    }
+
+    getCatById(idCat) {
+        fetch(`${this._url}/show/${idCat}`, {
+            method: 'GET',
+        }).then(this._onResponce);
+    }
+}
+
+const api = new Api (CONFIG_API);
+api.getAllCats();
---
+++ script/index.js
@@ -0,0 +1,59 @@
+const cardsContainer = document.querySelector('.cards');
+const btnOpenPopupForm = document.querySelector('#add');
+const btnOpenPopupLogin = document.querySelector('#login');
+const formCatAdd = document.querySelector('#popup-form-cat');
+
+const popupAddCat = new Popup('popup-add-cats');
+popupAddCat.setEventListener();
+
+const popupLogin = new Popup('popup-login');
+popupLogin.setEventListener();
+
+btnOpenPopupForm.addEventListener('click', () => {
+  popupAddCat.open();
+});
+
+formCatAdd.addEventListener('submit', handleFormAddCat);
+btnOpenPopupLogin.addEventListener('click', () => popupLogin.open());
+
+function serializeForm(elements) {
+    const formData = {};
+
+    elements.forEach((input) => {
+        if (input.type === 'submit') {
+            return
+        }
+
+        if (input.type !== 'checkbox') {
+            formData[input.name] = input.value;
+        }
+
+        if (input.type === 'checkbox') {
+            formData[input.name] = input.checked;
+        }
+    })
+    
+    return formData;
+}
+
+function createCat(dataCat) {
+    const cardInstance = new Card(dataCat, '#card-template');
+    const newCardElement = cardInstance.getElement();
+    cardsContainer.append(newCardElement);
+}
+
+function handleFormAddCat(e) {
+    e.preventDefault();
+    const elementsFormCat = [...formCatAdd.elements];
+    const dataFromForm = serializeForm(elementsFormCat);
+    api.addNewCat(dataFromForm).then(() => {
+        createCat(dataFromForm);
+    })
+    popupAddCat.close();
+}
+
+api.getAllCats().then((data) => {
+    data.forEach(function (catData) {
+        createCat(catData);
+    });
+})
